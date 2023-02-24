5fe5e8a5fa53dfbdcc0af7c44678b64ec58855f0
akinyemia <junior2inf@yahoo.com>
2023-02-23 16:52:53 -0800

    updated sql files

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -15,8 +15,7 @@ where EXTRACT(YEAR FROM hire_date) = '1986';
 --List the manager of each department along with 
 --their department number, department name, 
 --employee number, last name, and first name.
-select dm.dept_no, d.dept_name, e.emp_no, e.first_name, e.last_name  
-select *
+select dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name  
 from dept_manager dm
 left join employees e on dm.emp_no = e.emp_no
 left join departments d on dm.dept_no = d.dept_no; 
@@ -24,17 +23,11 @@ left join departments d on dm.dept_no = d.dept_no;
 --List the department number for each employee 
 --along with that employee’s employee number, 
 --last name, first name, and department name.
--- select de.dept_no, e.emp_no, e.last_name, e.first_name,d.dept_name  
--- select de.dept_no, e.emp_no, e.last_name, e.first_name 
-select *
+--emlpoyees work in more than 1 department
+select de.dept_no, e.emp_no, e.last_name, e.first_name,d.dept_name  
 from employees e
 left join dept_emp de on de.emp_no = e.emp_no
 left join departments d on de.dept_no = d.dept_no
-
-select * from dept_emp de
-left join departments d on de.dept_no = d.dept_no
-
-
 
 --List first name, last name, and sex of each 
 --employee whose first name is Hercules and 
@@ -49,19 +42,32 @@ where first_name = 'Hercules'
 --and first name.
 select e.emp_no, e.last_name, e.first_name 
 from employees e 
-left join
-
-alter table dept_emp
-alter column dept_no type VARCHAR(30)
+left join dept_emp de on de.emp_no = e.emp_no
+left join departments d on de.dept_no = d.dept_no 
+where de.dept_no = 'd007';
 
 --List each employee in the Sales and Development 
 --departments, including their employee number, 
 --last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name, d.dept_name 
+from employees e 
+left join dept_emp de on de.emp_no = e.emp_no
+left join departments d on de.dept_no = d.dept_no 
+where de.dept_no = 'd007' 
+and e.emp_no in (
+	select e.emp_no 
+	from employees e 
+	left join dept_emp de on de.emp_no = e.emp_no
+	left join departments d on de.dept_no = d.dept_no 
+	where de.dept_no = 'd005'
+);
 
 --List the frequency counts, in descending order, 
 --of all the employee last names (that is, how many employees share each last name).
-
+select last_name, count(last_name)
+from employees
+group by last_name
+order by count(*) desc;
 
 select * from departments
 select * from dept_manager
--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,30 +1,7 @@
--- create Employees table 
-create table Employees (
-	emp_no INT PRIMARY KEY, 
-	emp_title VARCHAR,
-	birthdate DATE,
-	first_name VARCHAR, 
-	last_name VARCHAR, 
-	sex VARCHAR, 
-	hire_date DATE
-);
-
 -- create Titles table 
 create table Titles (
 	title_id VARCHAR PRIMARY KEY, 
-	title VARCHAR
-);
-
--- create Dept_manager table 
-create table Dept_manager (
-	dept_no VARCHAR PRIMARY KEY, 
-	emp_no INT
-);
-
--- create Salaries table 
-create table Salaries (
-	emp_no INT PRIMARY KEY, 
-	salary INT
+	title VARCHAR 
 );
 
 -- create Departments table 
@@ -33,12 +10,38 @@ create table Departments (
 	dept_name VARCHAR
 );
 
+-- create Employees table 
+create table Employees (
+	emp_no INT PRIMARY KEY, 
+	emp_title VARCHAR REFERENCES Titles,
+	birthdate DATE,
+	first_name VARCHAR, 
+	last_name VARCHAR, 
+	sex VARCHAR, 
+	hire_date DATE
+);
+
+-- create Dept_manager table 
+create table Dept_manager (
+	dept_no VARCHAR REFERENCES Departments, 
+	emp_no INT REFERENCES Employees
+);
+
+-- create Salaries table 
+create table Salaries (
+	emp_no INT REFERENCES Employees, 
+	salary INT
+);
+
+
 -- create dept_emp table 
 create table Dept_emp (
-	emp_no INT PRIMARY KEY, 
-	dept_no VARCHAR
+	emp_no INT REFERENCES Employees, 
+	dept_no VARCHAR REFERENCES Departments
 );
 
 drop table employees;
 
 select * from employees; 
+
+REFERENCES products
